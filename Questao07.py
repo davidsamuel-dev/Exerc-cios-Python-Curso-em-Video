37400165adab9c683969bed0d27a742b1c4283d5
davidsamuel-dev <davidsamuel.dev@gmail.com>
2024-07-25 16:30:59 -0300

    envio

--- Questao07.py
+++ Questao07.py
@@ -1,1 +1,6 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = int(input("Informe a 1° nota: "))
+n2 = int(input("Informe a 2° nota: "))
+
+print(f"A Média deste aluno é: {(n1 + n2) / 2}")
