b02a6ec2d2126b2cfc3a3f2e11215e99229de6b9
davidsamuel-dev <davidsamuel.dev@gmail.com>
2024-07-26 12:33:29 -0300

    envio das questoes

--- Questao08.py
+++ Questao08.py
@@ -1,1 +1,8 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+valor = float(input("Informe um valor em metros: "))
+
+centimetros =  valor*100
+milimetros = valor*1000
+
+print(f"{valor} convertido para centimetros é: {centimetros} e para milimetros é: {milimetros}")
--- Questao10.py
+++ Questao10.py
@@ -1,1 +1,8 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere US$1,00 = R$3,27
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
+# e mostre quantos Dólares ela pode comprar. Considere US$1,00 = R$3,27
+import math
+carteira = float(input("Informe quntos você tem na sua carteira: "))
+
+dolares = carteira / 5.66
+
+print(f"Você pode comprar US$ {round(dolares, 2)}")
