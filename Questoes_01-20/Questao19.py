4e0e906120b6ca667e2fbe279efa076312b78d21
David Samuel Dias de Jesus <davidsamuel.dev@gmail.com>
2024-08-14 21:59:49 -0300

    Update Questao19.py
    xnvcbn

--- Questoes_01-20/Questao19.py
+++ Questoes_01-20/Questao19.py
@@ -1,2 +1,10 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+alunos = []
+qtd = int(input("Informe quantos alunos irá sortear: "))
+for i in range(0, qtd):
+    alunos.append(input(f"Informe o nome {i+1}° do aluno: "))
+
+print(alunos)
+
