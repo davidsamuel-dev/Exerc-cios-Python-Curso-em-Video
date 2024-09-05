a9bc3706f380137e29a3d864cbc577b62523a911
David Samuel Dias de Jesus <davidsamuel.dev@gmail.com>
2024-09-04 21:33:29 -0300

    Update Questao20.py

--- Questoes_01-20/Questao20.py
+++ Questoes_01-20/Questao20.py
@@ -1,3 +1,14 @@
 # O mesmo professor do exercício anterior quer sortear a ordem de apresentação 
 # de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos 
 # e mostre a ordem sorteada.
+
+import random
+
+alunos = []
+qtd = int(input("Informe quantos alunos irá sortear: "))
+for i in range(0, qtd):
+    alunos.append(input(f"Informe o nome {i+1}° do aluno: "))
+
+sorteado = random.choice(alunos)
+
+print("O aluno sorteado foi: ", sorteado)
